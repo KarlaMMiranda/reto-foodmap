7c06893b7f4649380b36feeb71d7718958ee285d
KarlaMMiranda <charlotterougue@gmail.com>
2018-01-30 15:43:24 -0600

    actualizando js

--- js/app.js
+++ js/app.js
@@ -1,11 +1,79 @@
-$(document).ready(function() {  //función que hace desaparecer mi imagen principal
+//Función que hace desaparecer mi imagen principal
+$(document).ready(function() {
   setTimeout(function() {
       $("#view-splash").fadeOut(1500);
     },2000);
 });
 
-$(document).ready(function() { //función que hace aparecer la siguiente pantalla
+//función que hace aparecer la siguiente pantalla
+$(document).ready(function() {
     setTimeout(function() {
       $("#second-section").fadeIn(1500);
     },2000);
 });
+
+//Función que filtra los resultados del mapa
+var map;
+
+  function initMap() {
+  var pyrmont = {lat: 19.420287, lng: -99.163119};
+
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: pyrmont,
+    zoom: 15
+  });
+
+  var service = new google.maps.places.PlacesService(map);
+  service.nearbySearch({
+    location: pyrmont,
+    radius: 1000,
+    type: ['restaurant'],
+    keyword: ['tacos'],
+  }, processResults);
+}
+
+function processResults(results, status, pagination) {
+  if (status !== google.maps.places.PlacesServiceStatus.OK) {
+    return;
+    } else {
+      createMarkers(results);
+
+    if (pagination.hasNextPage) {
+    var moreButton = document.getElementById('more');
+
+      moreButton.disabled = false;
+
+      moreButton.addEventListener('click', function() {
+      moreButton.disabled = true;
+      pagination.nextPage();
+      });
+    }
+  }
+}
+
+function createMarkers(places) {
+  var bounds = new google.maps.LatLngBounds();
+  var placesList = document.getElementById('places');
+
+  for (var i = 0, place; place = places[i]; i++) {
+    var image = {
+    url: place.icon,
+    size: new google.maps.Size(71, 71),
+    origin: new google.maps.Point(0, 0),
+    anchor: new google.maps.Point(17, 34),
+    scaledSize: new google.maps.Size(25, 25)
+  };
+
+  var marker = new google.maps.Marker({
+    map: map,
+    icon: image,
+    title: place.name,
+    position: place.geometry.location
+  });
+
+  placesList.innerHTML += '<li>' + place.name + '</li>';
+
+  bounds.extend(place.geometry.location);
+    }
+  map.fitBounds(bounds);
+  }
